878596a5dcecfab7c81687c46c2f8be769a99dcf
FattahHanafi <fattahhs@bhfsolution.com>
2024-10-03 22:38:56 -0400

    Added Realsense dockers

---
+++ docekr/dockerfiles/realsense.dockerfile
@@ -0,0 +1,10 @@
+FROM base_ros_image
+
+RUN apt-get install \
+  ros-$ROS_DISTRO-librealsense2* \
+  ros-$ROS_DISTRO-realsense2-* -y
+
+COPY ./entrypoints/d455_realsense.sh /ros_ws/
+COPY ./entrypoints/d435_realsense.sh /ros_ws/
+RUN chmod +x /ros_ws/d455_realsense.sh
+RUN chmod +x /ros_ws/d435_realsense.sh
---
+++ docekr/entrypoints/d435_realsense.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /opt/ros/$ROS_DISTRO/setup.sh
+ros2 launch realsense2_camera rs_launch.py \
+  device_type:=d435 \
+  camera_name:=D435 \
+  pointcloud.enable:=true \
+  pointcloud.ordered_pc:=true \
+  align_depth:=true \
+  enable_infra:=true \
+  enable_color:=true \
+  enable_depth:=true \
+  depth_module.infra_profile:=1280x720x30 \
+  rgb_camera.color_profile:=1280x800x30 \
+  depth_module.depth_profile:=1280x720x30 \
+  enable_sync:=true \
+  initial_reset:=true 
